41208cccdbcb0649370f78fa1051f55fb6653b46
T16K <gt1ariga@gmail.com>
2023-08-21 14:21:50 -0300

    update

--- examples/uv_sensor/server.py
+++ examples/uv_sensor/server.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-WoT application to expose a Thing that provides UV sensor values.
-"""
-
 import json
 import logging
 import tornado.gen
@@ -12,33 +8,38 @@ from tornado.ioloop import IOLoop
 from wotpy.protocols.http.server import HTTPServer
 from wotpy.wot.servient import Servient
 from rdflib import Graph, Namespace, Literal
-from rdflib.namespace import RDF, RDFS
+from rdflib.namespace import RDF, RDFS, XSD
 
-# Constants
 HTTP_PORT = 9494
 ID_THING = "urn:esp32"
 UV_SENSOR = "uv"
 
-# Global data storage
 uv_data = None
 
-# Setup logger
 logging.basicConfig()
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-# RDF setup
 g = Graph()
-n = Namespace("http://wotpyrdfsetup.org/device/")
-Device = n.Device
-UVSensor = n.UVSensor
-hasReading = n.hasReading
-sensor1 = n.sensor1
 
-# Create the class hierarchy
-g.add((UVSensor, RDFS.subClassOf, Device))
+# SSN ontology definitions
+ssn = Namespace("http://www.w3.org/ns/ssn/")
+sosa = Namespace("http://www.w3.org/ns/sosa/")
+ex = Namespace("http://wotpyrdfsetup.org/device/")
 
-# Thing description
+Observation = sosa.Observation
+ResultTime = sosa.resultTime
+HasResult = sosa.hasResult
+Observes = sosa.observes
+
+UVSensor = ex.UVSensor  # A specific sensor instance
+UVObservation = ex.UVObservation  # An observation class
+UVValue = ex.UVValue  # Observed value
+
+g.add((UVSensor, RDF.type, ssn.System))
+g.add((UVObservation, RDFS.subClassOf, Observation))
+g.add((UVValue, RDFS.datatype, XSD.float))
+
 description = {
     "id": ID_THING,
     "name": ID_THING,
@@ -48,13 +49,11 @@ description = {
             "readOnly": False,
             "observable": True
         }
-        # Add more sensors as needed
     }
 }
 
 @tornado.gen.coroutine
 def read_uv():
-    """Custom handler for the 'UV' property."""
     logger.info("Reading UV data.")
     if uv_data is None:
         return
@@ -63,22 +62,21 @@ def read_uv():
 
 @tornado.gen.coroutine
 def write_uv(value):
-    """Custom handler for writing UV data."""
     global uv_data
     global g
     logger.info("Writing UV data.")
     uv_data = value
     logger.info("UV data updated to: {}".format(uv_data))
 
-    # Update the graph
-    g.remove((sensor1, hasReading, None))  # remove old reading
     uv_data_dict = json.loads(uv_data.decode("utf-8"))
-    g.add((sensor1, RDF.type, UVSensor))
-    g.add((sensor1, hasReading, Literal(uv_data_dict['uv'])))
 
-    print(g.serialize(format="turtle"))  # print the graph for debugging
+    observation_uri = ex["Observation{}".format(uv_data_dict['uv'])]
 
-# Add handlers for other sensors as needed
+    g.remove((UVSensor, Observes, None))
+    g.add((UVSensor, Observes, UVObservation))
+    g.add((observation_uri, HasResult, Literal(uv_data_dict['uv'], datatype=UVValue)))
+
+    print(g.serialize(format="turtle"))
 
 @tornado.gen.coroutine
 def start_server():
@@ -93,7 +91,6 @@ def start_server():
     exposed_thing = wot.produce(json.dumps(description))
     exposed_thing.set_property_read_handler(UV_SENSOR, read_uv)
     exposed_thing.set_property_write_handler(UV_SENSOR, write_uv)
-    # Add handlers for other sensors as needed
     exposed_thing.expose()
 
 if __name__ == "__main__":
